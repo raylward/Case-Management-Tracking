6167366ce1630b9d7ef2931c20b16bf774aad9f5
Rebekah Aylward <raylward@carestar.com>
2018-04-13 14:26:49 -0400

    dropdown n links

--- client/tracker/src/components/index.vue
+++ client/tracker/src/components/index.vue
@@ -8,8 +8,8 @@
             </nav>
             </div>
             <div class="content">
-                <h2>My Profile</h2> <br>
-                    <a href="user_profile_female.jpg"><img src="user_profile_female.jpg" width="150" height="150" title="ProfilePhoto" alt="ProfilePhoto"></a>
+                <h2>My Profile</h2> <br>                
+                <div class="info">
                 <h3>Name</h3>
                 <p>
                 email<br><br>
@@ -17,6 +17,7 @@
                 Primary Program <br><br>
                 Location    <br><br>
                 Supervisor </p>
+                </div>
             </div>
             <div class="footer">
                 <small>2018 footer</small>
